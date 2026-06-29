a90f4f8397b81849ed226d69c254cdb7575b5543
thomas209 <57539552+thomas209@users.noreply.github.com>
2026-06-29 20:53:04 -0300

    fix: nextauth route export

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,6 @@
-import { handlers } from "@/lib/auth";
-export const { GET, POST } = handlers;
 export const dynamic = "force-dynamic";
+
+import { handlers } from "@/lib/auth";
+
+export const GET = handlers.GET;
+export const POST = handlers.POST;
